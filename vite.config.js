b949eab0eb52762d9398ff4f9eba684ef77a5f0c
MacBook Pro <admin@MacBooks-MacBook-Pro-2.local>
2023-12-19 00:56:53 +0700

    remove

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
+export default defineConfig({
   plugins: [react()],
-  base: '/',
-  }
-
-  if (command !== 'server') {
-    config.base = '/cv-deploy/'
-  }
-
-  return config
 })
